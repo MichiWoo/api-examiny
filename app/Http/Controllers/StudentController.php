dc875f87fbbf7a8a0131fcd33b33917374209ac9
MichiWoo <michiwoo.web@gmail.com>
2023-08-26 22:54:48 -0600

    :sparkles: Show, update y delete terminados

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -56,35 +56,90 @@ class StudentController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Student $student)
+    public function show($id_student)
     {
-        //
+        try {
+
+            $student = Student::where('id', $id_student)->first();
+
+            if ($student == NULL)
+            {
+                return $this->error("Error, NO se encontró el registro.");
+            }
+            
+            $resource = new StudentResource($student);
+    
+            return $this->success('Estudiante actualizado correctamente.', [
+                'estudiante' => $resource
+            ]);
+
+        } catch (\Throwable $th) {
+            return $this->error("Error al mostrar el registro, error:{$th->getMessage()}.");
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Student $student)
+    public function update(Request $request, $id_student)
     {
-        //
+        try {
+            
+            $rules = [
+                'name' => 'string|max:255',
+            ];
+
+            $validator = Validator::make( $request->all(), $rules, $messages = [
+                'required' => 'El campo :attribute es requerido.',
+                'numeric' => 'El campo :attribute debe ser númerico.',
+                'string' => 'El campo :attribute debe ser tipo texto.',
+                'max' => 'El campo :attribute excede el tamaño requerido((:max).',
+                'date_format' => 'El campo :attribute debe tener formato fecha (Y-m-d) ó formato fecha hora (YYYY-MM-DD HH:mm:ss)',
+            ]);
+
+            if ($validator->fails()) {
+                $errors = $validator->errors()->all();
+                return $this->error("Error al actualizar el registro", $errors);
+            }
+
+            $student = Student::where('id', $id_student)->first();
+            
+            if ($student == NULL)
+            {
+                return $this->error("Error, NO se encontró el registro.");
+            }
+
+            $student->name = $request->name;
+            $student->save();
+
+            $resource = new StudentResource($student);
+    
+            return $this->success('Estudiante actualizado correctamente.', [
+                'estudiante' => $resource
+            ]);
+        } catch (\Throwable $th) {
+            return $this->error("Error al actualizar el registro, error:{$th->getMessage()}.");
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Student $student)
+    public function destroy($id_student)
     {
-        //
-    }
+        try {
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Student $student)
-    {
-        //
+            $student = Student::where('id', $id_student)->first();
+            
+            if ($student == NULL)
+            {
+                return $this->error("Error, NO se encontró el registro.");
+            }
+
+            $student->delete();
+
+            $resource = new StudentResource($student);
+    
+            return $this->success('Estudiante eliminado correctamente.', [
+                'estudiante' => $resource
+            ]);
+
+        } catch (\Throwable $th) {
+            return $this->error("Error al eliminar el registro, error:{$th->getMessage()}.");
+        }
     }
 }
